d85646c1cfb44a42a45902cf2977c2d411996817
Jaganath Ezhilarasu <87165655+Jagan-03@users.noreply.github.com>
2021-09-10 14:00:49 +0530

    Delete CSS - transform directory

--- CSS - transform/js/script.js
+++
@@ -1,9 +0,0 @@
-let handleHover = (name) => {
-    setTimeout(() => {
-        if(name === "none"){
-            document.getElementById("main").style.backgroundImage = `url(img/default.jpg)`;
-        } else {
-            document.getElementById("main").style.backgroundImage = `url(img/${name}.jpg)`;
-        }
-    }, 100);
-}
